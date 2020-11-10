d8322893da28abe2dc76d52cae5c2fdb17ddce01
Odatha <bandaraods@gmail.com>
2020-11-10 11:00:02 +0530

    Express session created on user

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -38,14 +40,23 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321")); //pasing a secret key as a parameter(signed cookie)
-
+//app.use(cookieParser("12345-67890-09876-54321")); //pasing a secret key as a parameter(signed cookie)
+app.use(
+  session({
+    //set up session middleware
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 //client has to be first authorized
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     //not authorized yet
     var authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -62,7 +73,7 @@ function auth(req, res, next) {
     var user = auth[0];
     var password = auth[1];
     if (user === "admin" && password === "password") {
-      res.cookie("user", "admin", { signed: true });
+      req.session.user = "admin";
       next();
     } else {
       var err = new Error("You are noy authenticated!");
@@ -72,7 +83,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       next();
     } else {
       var err = new Error("You are noy authenticated!");
